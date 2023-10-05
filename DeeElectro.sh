cee6b61cf53a334d7fc331749015cf7d60ee79de
Dee-putra <146921104+Dee-putra@users.noreply.github.com>
2023-10-05 20:01:11 +0800

    Add files via upload

---
+++ DeeElectro.sh
@@ -0,0 +1,69 @@
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+echo
+toilet -f big -F gay DEE ELECTRO
+echo $green"T4MPIL4N BY DEE"
+echo
+sleep 2
+echo $yellow"SILAKAN PILIH TOOLS NYA :"
+echo
+echo $blue"1.)NEW MODE (BUAT BARU)"
+echo
+echo $blue"2.)NAMA BERWARNA"
+echo
+echo $blue"3.)ANONYMOUS"
+echo
+echo $blue"4.)DARK FB"
+echo
+echo $red"5.)KELUAR SAJA !!!"
+echo
+echo $cyan
+read -p "SILAKAN PILIH" BRO
+
+if [ $BRO = 1 ] || [ $BRO = 1 ]
+then
+clear
+toilet -f big -F gay DEE ELECTRO
+echo $white"TERMUX WELCOME NEW BY DEE ELECTRO"
+sleep 2
+nano ../usr/etc/bach.bascrc
+fi
+
+if [ $BRO = 2 ] || [ $BRO = 2 ]
+then
+clear
+toilet -f big -F gay DEE ELECTRO
+python2 style5.py
+fi
+
+if [ $BRO = 3 ] || [ $BRO = 3 ]
+then
+clear
+toilet -f big -F gay DEE ELECTRO
+python2 style5.py
+fi
+
+if [ $BRO = 4 ] || [ $BRO = 4 ]
+then
+clear
+toilet -f big -F gay DEE ELECTRO
+python2 style5.py
+fi
+fi
+
+if [ $BRO = 5 ] || [ $BRO = 5 ]
+then
+clear
+echo $red
+figlet "BYE"
+echo $green"SAMPAI JUMPA"
+sleep 3
+exit
+fi
